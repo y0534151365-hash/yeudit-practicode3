415ed77cd0c46e41b969bca3ca839744e6d15a09
“y0534151365-hash” <y0534151365@gmail.com>
2025-11-23 01:33:55 +0200

    add3

--- server/Program.cs
+++ server/Program.cs
@@ -32,13 +32,28 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddAuthorization();
 
 // הגדרת CORS
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowAll", policy =>
+//     {
+//         policy.AllowAnyOrigin()
+//               .AllowAnyMethod()
+//               .AllowAnyHeader();
+//     });
+// });
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
     {
-        policy.AllowAnyOrigin()
+        policy.WithOrigins(
+                "https://grayover.onrender.com",
+                "http://localhost:3000",
+                "https://yeudit-practicode3-lastserver.onrender.com"
+              )
               .AllowAnyMethod()
-              .AllowAnyHeader();
+              .AllowAnyHeader()
+              .AllowCredentials();
     });
 });
 
@@ -86,15 +101,15 @@ builder.Services.AddDbContext<ToDoDbContext>(options =>
 var app = builder.Build();
 
 // Swagger בפיתוח בלבד
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "ToDo API v1");
         c.RoutePrefix = string.Empty;
     });
-}
+// }
 
 app.UseCors("AllowAll");
 app.UseAuthentication();
