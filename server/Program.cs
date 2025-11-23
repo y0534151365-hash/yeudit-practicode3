5536cc0ec29cb133793c978ba5df743605460d91
“y0534151365-hash” <y0534151365@gmail.com>
2025-11-23 02:00:16 +0200

    add5

--- server/Program.cs
+++ server/Program.cs
@@ -122,6 +122,17 @@ var app = builder.Build();
 // }
 
 app.UseCors("AllowAll");
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == "OPTIONS")
+    {
+        context.Response.StatusCode = 200;
+        await context.Response.CompleteAsync();
+        return;
+    }
+    await next();
+});
 app.UseAuthentication();
 app.UseAuthorization();
 
